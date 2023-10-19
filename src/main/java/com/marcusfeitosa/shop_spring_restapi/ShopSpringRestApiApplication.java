86e234255253308b3a59e99ff59b46efa22d115e
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-19 16:36:17 -0300

    Initial commit

---
+++ src/main/java/com/marcusfeitosa/shop_spring_restapi/ShopSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.marcusfeitosa.shop_spring_restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopSpringRestApiApplication.class, args);
+	}
+
+}
