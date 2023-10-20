64b2cd8dde72fcadca81fdb9e732305aad75e2d1
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-20 10:27:39 -0300

    Creating Order class and User relation_V2

--- src/main/java/com/marcusfeitosa/shop_spring_restapi/entities/Order.java
+++ src/main/java/com/marcusfeitosa/shop_spring_restapi/entities/Order.java
@@ -1,12 +1,24 @@
 package com.marcusfeitosa.shop_spring_restapi.entities;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.Instant;
-@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_order")
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
     private User client;
 
 }
