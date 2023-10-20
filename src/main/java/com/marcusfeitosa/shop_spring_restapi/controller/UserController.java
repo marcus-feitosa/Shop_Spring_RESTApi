b997915e0501d0e2d3da9a439f93a501cc86502e
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-10-20 09:32:13 -0300

    Creating findAll() and findById() methods

--- src/main/java/com/marcusfeitosa/shop_spring_restapi/controller/UserController.java
+++ src/main/java/com/marcusfeitosa/shop_spring_restapi/controller/UserController.java
@@ -1,26 +1,33 @@
 package com.marcusfeitosa.shop_spring_restapi.controller;
 
 import com.marcusfeitosa.shop_spring_restapi.entities.User;
+import com.marcusfeitosa.shop_spring_restapi.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserService service;
     @GetMapping("/get")
-    public ResponseEntity<User> findAll(){
-       User u = new User(
-               1L,
-               "Maria",
-               "maria@gmail.com",
-               "9999999",
-               "12345"
-       );
+    public ResponseEntity<List<User>> findAll(){
+        List<User> u = service.findAll();
+
        return ResponseEntity.ok().body(u);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
--- src/main/java/com/marcusfeitosa/shop_spring_restapi/service/UserService.java
+++ src/main/java/com/marcusfeitosa/shop_spring_restapi/service/UserService.java
@@ -1,7 +1,26 @@
 package com.marcusfeitosa.shop_spring_restapi.service;
 
+import com.marcusfeitosa.shop_spring_restapi.entities.User;
+import com.marcusfeitosa.shop_spring_restapi.repositories.UserInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+@Service
 public class UserService {
 
+    @Autowired
+    private UserInterface repository;
 
+    public List<User> findAll(){
+        return repository.findAll();
+    }
 
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
 }
